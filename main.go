139ba1dbf55e013cbb51613f98137903790f176b
nicholasss <nicholaszingleman@gmail.com>
2025-04-30 19:52:29 -0400

    added create user handler and stubbed respond with error function

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"log"
 	"net/http"
 	"os"
@@ -18,6 +19,16 @@ type apiConfig struct {
 	db *database.Queries
 }
 
+// response types
+
+type createUserRequest struct {
+	CreatedBy   string `json:"createdBy"`
+	UpdatedBy   string `json:"updatedBy"`
+	IsAdmin     bool   `json:"isAdmin"`
+	Email       string `json:"email"`
+	RawPassword string `json:"rawPassword"`
+}
+
 // === Middleware Functions ===
 
 func (cfg *apiConfig) logMW(next http.Handler) http.Handler {
@@ -27,12 +38,40 @@ func (cfg *apiConfig) logMW(next http.Handler) http.Handler {
 	})
 }
 
+// === Utility Response Handlers ===
+
+func respondWithError(err error, w http.ResponseWriter) {
+}
+
 // === Handler Functions ===
 
 func healthHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("OK"))
+}
+
+func (cfg *apiConfig) createUserHandler(w http.ResponseWriter, r *http.Request) {
+	var createUserRequest createUserRequest
+	decoder := json.NewDecoder(r.Body)
+	err := decoder.Decode(&createUserRequest)
+	if err != nil {
+		// respond with error
+	}
+
+	// check request params
+	if createUserRequest.Email == "" {
+		// respond with error
+	}
+	if createUserRequest.RawPassword == "" {
+		// respond with error
+	}
+	if createUserRequest.CreatedBy == "" {
+		// respond with error
+	}
+	if createUserRequest.UpdatedBy == "" {
+		// respond with error
+	}
 }
 
 // === Main Function ===
@@ -62,7 +101,11 @@ func main() {
 
 	mux := http.NewServeMux()
 
+	// health endpoint
 	mux.Handle("GET /health", cfg.logMW(http.HandlerFunc(healthHandler)))
+
+	// user endpoints
+	mux.Handle("POST /api/v1/createuser", cfg.logMW(http.HandlerFunc(cfg.createUserHandler)))
 
 	log.Printf("Server is now online.\n")
 	log.Fatal(http.ListenAndServe(":8080", mux))
