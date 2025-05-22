1d4c04cd90f2ed17b0a112391c21ae668c9fd55d
Nicholas <nicholasss@users.noreply.github.com>
2025-05-22 10:56:00 -0400

    added plant names table

---
+++ sql/schema/004_plant_names.sql
@@ -0,0 +1,32 @@
+-- +goose Up
+create table plant_names (
+	id uuid primary key,
+  created_at timestamp with time zone not null,
+  updated_at timestamp with time zone not null,
+  deleted_at timestamp with time zone,
+  --
+  created_by text not null,
+  updated_by text not null,
+  deleted_by text,
+  --
+  -- foreign keys
+  plant_id uuid,
+  --
+  -- table data
+  lang_code text,
+  common_name text
+);
+
+alter table plant_names
+  add constraint fk_plants
+  foreign key (plant_id)
+  references plants(id)
+  on delete cascade;
+
+-- +goose Down
+alter table plant_names
+	drop constraint fk_plants;
+
+drop table plant_names;
+
+
