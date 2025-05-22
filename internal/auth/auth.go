5209bbac801d4aabde75c2ead4fbe914ac44a92f
Nicholas <nicholasss@users.noreply.github.com>
2025-05-22 15:15:29 -0400

    added jwt funcs to auth and added jwt package

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -2,13 +2,18 @@ package auth
 
 import (
 	"crypto/hmac"
+	"crypto/rand"
 	"encoding/base64"
+	"encoding/hex"
 	"errors"
 	"fmt"
 	"log"
 	"net/http"
 	"strings"
+	"time"
 
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -66,6 +71,70 @@ func GetBearerToken(headers http.Header) (string, error) {
 
 	log.Printf("Returned the JWT successfuly from headers.\n")
 	return tokenString, nil
+}
+
+// creates and returns a JWT
+func MakeJWT(userID uuid.UUID, tokenSecret string, expiresIn time.Duration) (string, error) {
+	currentTime := time.Now().UTC()
+	expirationTime := currentTime.UTC().Add(expiresIn)
+
+	signingMethod := jwt.SigningMethodHS256
+	claims := jwt.RegisteredClaims{
+		Issuer:    "chirpy",
+		IssuedAt:  jwt.NewNumericDate(currentTime),
+		ExpiresAt: jwt.NewNumericDate(expirationTime),
+		Subject:   userID.String(),
+	}
+	token := jwt.NewWithClaims(signingMethod, claims)
+
+	// HMAC signing method requires the type []byte
+	signedToken, err := token.SignedString([]byte(tokenSecret))
+	if err != nil {
+		log.Printf("Error signing JWT: %s", err)
+		return "", err
+	}
+
+	// log.Printf("Generated new token: %s", signedToken)
+	return signedToken, nil
+}
+
+func ValidateJWT(tokenString, tokenSecret string) (uuid.UUID, error) {
+	claims := jwt.RegisteredClaims{}
+
+	token, err := jwt.ParseWithClaims(tokenString, &claims,
+		func(token *jwt.Token) (any, error) {
+			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+				return uuid.Nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+			}
+			return []byte(tokenSecret), nil
+		})
+	if err != nil {
+		return uuid.Nil, err
+	}
+
+	userID, err := token.Claims.GetSubject()
+	if err != nil {
+		return uuid.Nil, err
+	}
+
+	userUUID, err := uuid.Parse(userID)
+	if err != nil {
+		return uuid.Nil, err
+	}
+
+	return userUUID, nil
+}
+
+// returns a created refresh token
+func MakeRefreshToken() (string, error) {
+	data := make([]byte, 32)
+	_, err := rand.Read(data)
+	if err != nil {
+		return "", err
+	}
+
+	secureString := hex.EncodeToString(data)
+	return secureString, nil
 }
 
 // === User Password Functions ===
