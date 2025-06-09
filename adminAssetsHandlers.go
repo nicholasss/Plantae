b1ed9da57f00410250a48388656b3c8a1bd791f9
Nicholas <nicholasss@users.noreply.github.com>
2025-06-08 21:26:45 -0500

    created new response type and big conversion for plant list

--- adminAssetsHandlers.go
+++ adminAssetsHandlers.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/google/uuid"
 	"github.com/nicholasss/plantae/internal/auth"
@@ -22,6 +23,19 @@ type AdminPlantsCreateRequest struct {
 	PetPoisonToxic   *bool  `json:"petPoisonToxic,omitempty"`
 	HumanEdible      *bool  `json:"humanEdible,omitempty"`
 	PetEdible        *bool  `json:"petEdible,omitempty"`
+}
+
+type AdminPlantsViewResponse struct {
+	ID               uuid.UUID `json:"id"`
+	CreatedAt        time.Time `json:"createdAt"`
+	UpdatedAt        time.Time `json:"updatedAt"`
+	CreatedBy        string    `json:"createdBy"`
+	UpdatedBy        string    `json:"updatedBy"`
+	SpeciesName      string    `json:"speciesName"`
+	HumanPoisonToxic *bool     `json:"humanPoisonToxic,omitempty"`
+	PetPoisonToxic   *bool     `json:"petPoisonToxic,omitempty"`
+	HumanEdible      *bool     `json:"humanEdible,omitempty"`
+	PetEdible        *bool     `json:"petEdible,omitempty"`
 }
 
 // === helper functions ===
@@ -70,17 +84,20 @@ func (cfg *apiConfig) resetPlantSpeciesHandler(w http.ResponseWriter, r *http.Re
 func (cfg *apiConfig) adminPlantsViewHandler(w http.ResponseWriter, r *http.Request) {
 	requestUserID, err := cfg.authorizeNormalAdmin(r)
 	if err != nil {
+		log.Printf("Could not authorize normal (non-superadmin) due to: %q", err)
 		respondWithError(err, http.StatusBadRequest, w)
 		return
 	}
 
 	userRecord, err := cfg.db.GetUserByIDWithoutPassword(r.Context(), requestUserID)
 	if err != nil {
+		log.Printf("Could not get user record via user id due to: %q", err)
 		respondWithError(err, http.StatusBadRequest, w)
 		return
 	}
 
 	if !userRecord.IsAdmin {
+		log.Print("Could not view plants due to user not being admin.")
 		respondWithError(fmt.Errorf("unauthorized"), http.StatusUnauthorized, w)
 		return
 	}
@@ -88,6 +105,7 @@ func (cfg *apiConfig) adminPlantsViewHandler(w http.ResponseWriter, r *http.Requ
 
 	plantSpeciesRecords, err := cfg.db.GetAllPlantSpeciesOrderedByCreated(r.Context())
 	if err != nil {
+		log.Printf("Could not get plant species records due to: %q", err)
 		respondWithError(err, http.StatusInternalServerError, w)
 		return
 	}
@@ -95,18 +113,66 @@ func (cfg *apiConfig) adminPlantsViewHandler(w http.ResponseWriter, r *http.Requ
 	// log.Printf("plants list: %#v", plantSpeciesRecords)
 
 	if len(plantSpeciesRecords) <= 0 {
-		log.Printf("Admin listed empty plant species list successfully.")
+		log.Print("Admin listed empty plant species list successfully.")
 		w.WriteHeader(http.StatusNoContent)
 		return
 	}
 
-	plantSpeciesData, err := json.Marshal(plantSpeciesRecords)
+	plantSpeciesResponse := make([]AdminPlantsViewResponse, 0)
+	for _, oldRecord := range plantSpeciesRecords {
+		var humanPT *bool
+		if oldRecord.HumanPoisonToxic.Valid {
+			humanPT = &oldRecord.HumanPoisonToxic.Bool
+		} else {
+			humanPT = nil
+		}
+
+		var humanE *bool
+		if oldRecord.HumanEdible.Valid {
+			humanE = &oldRecord.HumanEdible.Bool
+		} else {
+			humanE = nil
+		}
+
+		var petPT *bool
+		if oldRecord.PetPoisonToxic.Valid {
+			petPT = &oldRecord.PetPoisonToxic.Bool
+		} else {
+			petPT = nil
+		}
+
+		var petE *bool
+		if oldRecord.PetEdible.Valid {
+			petE = &oldRecord.PetEdible.Bool
+		} else {
+			petE = nil
+		}
+
+		newResponse := AdminPlantsViewResponse{
+			ID:               oldRecord.ID,
+			CreatedAt:        oldRecord.CreatedAt,
+			UpdatedAt:        oldRecord.UpdatedAt,
+			CreatedBy:        oldRecord.CreatedBy,
+			UpdatedBy:        oldRecord.UpdatedBy,
+			SpeciesName:      oldRecord.SpeciesName,
+			HumanPoisonToxic: humanPT,
+			HumanEdible:      humanE,
+			PetPoisonToxic:   petPT,
+			PetEdible:        petE,
+		}
+
+		plantSpeciesResponse = append(plantSpeciesResponse, newResponse)
+	}
+
+	plantSpeciesData, err := json.Marshal(plantSpeciesResponse)
 	if err != nil {
+		log.Printf("Could not get plant_species records due to: %q", err)
 		respondWithError(err, http.StatusInternalServerError, w)
 		return
 	}
 
 	log.Printf("Admin listed plant species list successfully.")
+	log.Printf("DEBUG: list of plants: %s", string(plantSpeciesData))
 	w.WriteHeader(http.StatusOK)
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(plantSpeciesData)
@@ -117,6 +183,7 @@ func (cfg *apiConfig) adminAllInfoPlantsCreateHandler(w http.ResponseWriter, r *
 	// check header for admin access token
 	requestUserID, err := cfg.authorizeNormalAdmin(r)
 	if err != nil {
+		log.Printf("Could not authorize normal (non-superadmin) due to: %q", err)
 		respondWithError(err, http.StatusBadRequest, w)
 		return
 	}
@@ -124,6 +191,7 @@ func (cfg *apiConfig) adminAllInfoPlantsCreateHandler(w http.ResponseWriter, r *
 	var createRequest AdminPlantsCreateRequest
 	err = json.NewDecoder(r.Body).Decode(&createRequest)
 	if err != nil {
+		log.Printf("Could not decode body of request due to: %q", err)
 		respondWithError(err, http.StatusBadRequest, w)
 		return
 	}
@@ -131,20 +199,23 @@ func (cfg *apiConfig) adminAllInfoPlantsCreateHandler(w http.ResponseWriter, r *
 
 	// check all of the request properties
 	if createRequest.Client == "" {
+		log.Print("Request body was missing client field.")
 		respondWithError(errors.New("no client name provided"), http.StatusBadRequest, w)
 		return
 	}
 	if createRequest.SpeciesName == "" {
+		log.Print("Request body was missing species name.")
 		respondWithError(errors.New("no species name provided"), http.StatusBadRequest, w)
 		return
 	}
 
-	// NOTE: convert to a warning if a single field is missing?
+	// NOTE: convert to a warning if a single field is missing instead of individual?
 	humanPoisonToxic := sql.NullBool{}
 	if createRequest.HumanPoisonToxic == nil {
 		log.Print("WARNING: human_poison_toxic field missing.")
 		humanPoisonToxic.Valid = false
 	} else {
+		log.Print("Request body has human_poison_toxic is present.")
 		humanPoisonToxic.Valid = true
 		humanPoisonToxic.Bool = *createRequest.HumanPoisonToxic
 	}
@@ -154,6 +225,7 @@ func (cfg *apiConfig) adminAllInfoPlantsCreateHandler(w http.ResponseWriter, r *
 		log.Print("WARNING: pet_poison_toxic field missing.")
 		petPoisonToxic.Valid = false
 	} else {
+		log.Print("Request body has pet_poison_toxic is present.")
 		petPoisonToxic.Valid = true
 		petPoisonToxic.Bool = *createRequest.PetPoisonToxic
 	}
@@ -163,6 +235,7 @@ func (cfg *apiConfig) adminAllInfoPlantsCreateHandler(w http.ResponseWriter, r *
 		log.Print("WARNING: human_edible field missing.")
 		humanEdible.Valid = false
 	} else {
+		log.Print("Request body has human_edible is present.")
 		humanEdible.Valid = true
 		humanEdible.Bool = *createRequest.HumanEdible
 	}
@@ -172,6 +245,7 @@ func (cfg *apiConfig) adminAllInfoPlantsCreateHandler(w http.ResponseWriter, r *
 		log.Print("WARNING: pet_edible field missing.")
 		petEdible.Valid = false
 	} else {
+		log.Print("Request body has pet_edible is present.")
 		petEdible.Valid = true
 		petEdible.Bool = *createRequest.PetEdible
 	}
@@ -187,6 +261,7 @@ func (cfg *apiConfig) adminAllInfoPlantsCreateHandler(w http.ResponseWriter, r *
 	}
 	_, err = cfg.db.CreatePlantSpecies(r.Context(), createRequestParams)
 	if err != nil {
+		log.Printf("Could not create plant_species record in database due to %q.", err)
 		respondWithError(err, http.StatusInternalServerError, w)
 		return
 	}
