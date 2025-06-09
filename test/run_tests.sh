e1b0ab5589e20a6908665c655817fffe77992881
Nicholas <nicholasss@users.noreply.github.com>
2025-06-08 20:07:40 -0500

    added additional variable and numbering

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -21,11 +21,12 @@ source .env
 hurl \
   --variable lisa_email=lisa@gmail.com \
   --variable lisa_password=Growl1ng! \
-  --variable species_name="Pilea peperomioides" \
-  --variable human_poison_toxic=false \
-  --variable pet_poison_toxic=false \
-  --variable human_edible=false \
-  --variable pet_edible=false \
+  --variable 1_species_name="Pilea peperomioides" \
+  --variable 1_human_poison_toxic=false \
+  --variable 1_pet_poison_toxic=false \
+  --variable 1_human_edible=false \
+  --variable 1_pet_edible=false \
+  --variable 2_species_name="Crassula ovata" \
   --secret super_admin_token=$SUPER_ADMIN_TOKEN \
   --test \
   test/admin.hurl
