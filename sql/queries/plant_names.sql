304f3809cc4c19acc537c6a3b28ca8e0e726ed57
Nicholas <nicholasss@users.noreply.github.com>
2025-06-13 16:14:34 -0400

    added queries for plant_names table

---
+++ sql/queries/plant_names.sql
@@ -0,0 +1,26 @@
+-- name: CreatePlantName :one
+insert into plant_names (
+	id,
+  created_at, updated_at,
+	created_by, updated_by,
+  plant_id,
+  lang_code,
+  common_name
+) values (
+  gen_random_uuid(),
+  now(), now(),
+  $1, $1,
+  $2,
+  $3,
+  $4
+) returning *;
+
+-- name: ResetPlantNamesTable :exec
+delete from plant_names;
+
+-- name: MarkPlantNameAsDeletedByID :exec
+update plant_names
+  set
+  deleted_at = now(),
+  deleted_by = $2
+where id = $1;
