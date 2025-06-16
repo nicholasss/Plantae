ade9b68cb21fde002321be9e1ac342548509f1dd
Nicholas <nicholasss@users.noreply.github.com>
2025-06-16 10:57:35 -0400

    renamed plant_type to plant_types

--- sql/schema/003_plant_type.sql
+++ sql/schema/003_plant_type.sql
@@ -1,5 +1,5 @@
 -- +goose Up
-create table plant_type (
+create table plant_types (
   id uuid primary key,
   created_at timestamp with time zone not null,
   updated_at timestamp with time zone not null,
@@ -27,15 +27,15 @@ alter table plant_species
   add column plant_type_id uuid;
 
 alter table plant_species
-	add constraint fk_plant_type
+	add constraint fk_plant_types
 	foreign key (plant_type_id)
-	references plant_type(id);
+	references plant_types(id);
 
 -- +goose Down
 alter table plant_species
-	drop constraint fk_plant_type;
+	drop constraint fk_plant_types;
 
 alter table plant_species
   drop column plant_type_id;
 
-drop table plant_type;
+drop table plant_types;
