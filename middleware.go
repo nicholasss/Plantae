3febc368ca3b6eac325ad6041a2ba947bfe350e8
Nicholas <nicholasss@users.noreply.github.com>
2025-06-23 15:43:47 -0400

    chore: passing in logger

--- middleware.go
+++ middleware.go
@@ -14,13 +14,13 @@ func (cfg *apiConfig) authSuperAdminMW(next http.Handler) http.Handler {
 		requestToken, err := auth.GetAuthKeysValue(r.Header, "SuperAdminToken", cfg.sl)
 		if err != nil {
 			cfg.sl.Debug("Unable to get superadmin token from headers", "error", err)
-			respondWithError(err, http.StatusBadRequest, w)
+			respondWithError(err, http.StatusBadRequest, w, cfg.sl)
 			return
 		}
 
 		if ok := auth.ValidateSuperAdmin(cfg.superAdminToken, requestToken, cfg.sl); !ok {
 			cfg.sl.Debug("Unable to validate superadmin token in request")
-			respondWithError(err, http.StatusForbidden, w)
+			respondWithError(err, http.StatusForbidden, w, cfg.sl)
 			return
 		}
 
@@ -35,20 +35,20 @@ func (cfg *apiConfig) authNormalAdminMW(next http.Handler) http.Handler {
 		requestUserID, err := cfg.getUserIDFromToken(r)
 		if err != nil {
 			cfg.sl.Debug("Could not authorize user in request", "error", err)
-			respondWithError(err, http.StatusBadRequest, w)
+			respondWithError(err, http.StatusBadRequest, w, cfg.sl)
 			return
 		}
 
 		userRecord, err := cfg.db.GetUserByIDWithoutPassword(r.Context(), requestUserID)
 		if err != nil {
 			cfg.sl.Debug("Could not find user record", "user id", requestUserID, "error", err)
-			respondWithError(err, http.StatusInternalServerError, w)
+			respondWithError(err, http.StatusInternalServerError, w, cfg.sl)
 			return
 		}
 
 		if !userRecord.IsAdmin {
 			cfg.sl.Debug("Non-Admin is performing requests to admin endpoints", "email", userRecord.Email, "id", requestUserID)
-			respondWithError(err, http.StatusUnauthorized, w)
+			respondWithError(err, http.StatusUnauthorized, w, cfg.sl)
 			return
 		}
 
