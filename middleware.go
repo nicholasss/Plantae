2a82f23796953153ffb900db6881794c27615ec0
Nicholas <nicholasss@users.noreply.github.com>
2025-06-30 15:06:01 -0400

    fix: added queries to middleware logging

--- middleware.go
+++ middleware.go
@@ -59,7 +59,7 @@ func (cfg *apiConfig) authNormalAdminMW(next http.Handler) http.Handler {
 
 func (cfg *apiConfig) logMW(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		cfg.sl.Debug("Incoming request", "method", r.Method, "path", r.URL.Path)
+		cfg.sl.Debug("Incoming request", "method", r.Method, "path", r.URL.Path, "queries", r.URL.RawQuery)
 		next.ServeHTTP(w, r)
 	})
 }
